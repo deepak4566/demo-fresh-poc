30743769e8d5382ee206ec19446cdab4f8e83577
Deepak <deepakreddyats1@gmail.com>
2024-11-22 01:05:45 +0530

    Update Script.sh

--- Script.sh
+++ Script.sh
@@ -5,6 +5,8 @@ echo "I am starting the game now"
 
 # Check if the directory exists
 mkdir ppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppp
+
+cd demo-fresh-poc
 # Pull latest changes from git repository with sudo permissions
 sudo git pull || { echo "Git pull failed"; exit 1; }
 
