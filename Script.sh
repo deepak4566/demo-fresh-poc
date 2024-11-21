ee7131565d1e512568c2c37a12a97d8f1ba81bb1
Deepak <deepakreddyats1@gmail.com>
2024-11-22 01:01:16 +0530

    Update Script.sh

--- Script.sh
+++ Script.sh
@@ -4,14 +4,6 @@
 echo "I am starting the game now"
 
 # Check if the directory exists
-if [ -d "/var/www/html/poc-php.cottonseeds.org" ]; then
-  echo "/var/www/html/poc-php.cottonseeds.org exists"
-  # Try to change directory to the project folder
-  cd /var/www/html/poc-php.cottonseeds.org || { echo "Failed to enter the directory"; exit 1; }
-else
-  echo "/var/www/html/poc-php.cottonseeds.org does not exist"
-  exit 1
-fi
 
 # Pull latest changes from git repository with sudo permissions
 sudo git pull || { echo "Git pull failed"; exit 1; }
